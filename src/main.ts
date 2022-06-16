8c02e84f34804a040d5f494a053ff780772e57ff
xionglp <785874752@qq.com>
2022-06-16 12:30:06 +0800

    feat(element-plus): element-plus add

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import App from "./App.vue"
 import router from "@/router/index"
 import store from "@/store/index"
 
+import ElementPlus from "element-plus"
+import "element-plus/theme-chalk/index.css"
+
 const app = createApp(App)
 app.use(router)
 app.use(store)
+app.use(ElementPlus)
 app.mount("#app")
