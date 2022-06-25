ef601f972786fc827709998bd5bbd62ee75f74b2
xionglp <785874752@qq.com>
2022-06-25 20:15:16 +0800

    feat(usermenu): 左侧菜单栏的布局和显示

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,4 +1,12 @@
+import { ILoginState } from "./login/login-types"
+
 export interface IRootState {
   name: string
   age?: number
 }
+
+export interface IRootWithModule {
+  loginModule: ILoginState
+}
+
+export type IStoreType = IRootState & IRootWithModule
