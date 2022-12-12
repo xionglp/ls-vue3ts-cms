c9dc2aafb3e52b41abd489f09821999031f0a350
xionglp <785874752@qq.com>
2022-12-12 11:45:38 +0800

    feat(user): user page list header-footer-国际化zhCn

--- src/base-ui/table/src/table.vue
+++ src/base-ui/table/src/table.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="table">
-    <el-table :data="listData" style="width: 100%" border>
+    <div class="header">
+      <slot name="header">
+        <div class="title">{{ title }}</div>
+        <div class="handler">
+          <slot name="headerHandler"></slot>
+        </div>
+      </slot>
+    </div>
+    <el-table :data="listData" style="width: 100%" border @selection-change="handleSelectionChange">
+      <el-table-column v-if="showSelectColumn" type="selection" width="60" align="center"></el-table-column>
+      <el-table-column v-if="showIndexColumn" type="index" label="序号" width="80" align="center"></el-table-column>
       <template v-for="item in propList" :key="item.name">
         <el-table-column align="center" v-bind="item">
           <template #default="scope">
@@ -11,6 +21,20 @@
         </el-table-column>
       </template>
     </el-table>
+    <div class="footer">
+      <slot name="footer">
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage"
+          :page-sizes="[100, 200, 300, 400]"
+          :page-size="100"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="400"
+        >
+        </el-pagination>
+      </slot>
+    </div>
   </div>
 </template>
 
@@ -19,6 +43,10 @@ import { defineComponent } from "vue"
 
 export default defineComponent({
   props: {
+    title: {
+      type: String,
+      default: ""
+    },
     listData: {
       type: Array,
       required: true
@@ -26,10 +54,38 @@ export default defineComponent({
     propList: {
       type: Array as any,
       required: true
+    },
+    showIndexColumn: {
+      type: Boolean,
+      default: false
+    },
+    showSelectColumn: {
+      type: Boolean,
+      default: false
     }
   },
-  setup() {
-    return {}
+  emits: ["selectionChange"],
+  setup(props, { emit }) {
+    const currentPage = 2
+
+    const handleSelectionChange = (value: any) => {
+      console.log(value)
+      emit("selectionChange", value)
+    }
+
+    const handleSizeChange = (value: any) => {
+      console.log(value)
+    }
+
+    const handleCurrentChange = (value: any) => {
+      console.log(value)
+    }
+    return {
+      currentPage,
+      handleSelectionChange,
+      handleSizeChange,
+      handleCurrentChange
+    }
   }
 })
 </script>
@@ -38,6 +94,33 @@ export default defineComponent({
 .table {
   :deep(.el-table__row) {
     height: 55px;
+  }
+}
+
+.header {
+  display: flex;
+  height: 45px;
+  padding: 0 5px 5px;
+  justify-content: space-between;
+  align-items: center;
+
+  .title {
+    font-size: 20px;
+    font-weight: 700;
+  }
+
+  .handler {
+    align-items: center;
+  }
+}
+
+.footer {
+  display: flex;
+  justify-content: right;
+  margin-top: 15px;
+
+  .el-pagination {
+    text-align: right;
   }
 }
 </style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -2,7 +2,23 @@
   <div class="user">
     <page-search :searchFormConfig="searchFormConfig"></page-search>
     <div class="content">
-      <ls-table :propList="propList" :listData="userList">
+      <ls-table
+        :propList="propList"
+        :listData="userList"
+        :showIndexColumn="showIndexColumn"
+        :showSelectColumn="showSelectColumn"
+        :title="title"
+        @selectionChange="selectionChange"
+      >
+        <!-- 1. header中的插槽 -->
+        <template #headerHandler>
+          <el-button type="primary">
+            <el-icon class="el-icon--left"><CirclePlus /></el-icon>
+            新增
+          </el-button>
+        </template>
+
+        <!-- 2. 列中的插槽 -->
         <template #status="scope">
           <el-button size="small" plain :type="scope.row.enable ? 'success' : 'danger'">{{ scope.row.enable ? "启用" : "禁用" }}</el-button>
         </template>
@@ -12,6 +28,20 @@
         <template #updateAt="scope">
           <span>{{ $filters.formatTime(scope.row.updateAt) }}</span>
         </template>
+        <template #handler>
+          <div class="handle-btns">
+            <el-button link>
+              <el-icon class="el-icon--left"><EditPen /></el-icon>
+              编辑
+            </el-button>
+            <el-button link>
+              <el-icon class="el-icon--left"><Delete /></el-icon>
+              删除
+            </el-button>
+          </div>
+        </template>
+
+        <!-- 3. footer插槽， 不写就用内部默认的分页器 -->
       </ls-table>
     </div>
   </div>
@@ -42,6 +72,9 @@ export default defineComponent({
 
     const userList = computed(() => store.state.system.userList)
     const userCount = computed(() => store.state.system.userCount)
+    const showIndexColumn = true
+    const showSelectColumn = true
+    const title = "用户列表"
 
     const propList = [
       { prop: "name", label: "用户名", minWidth: "100" },
@@ -49,14 +82,23 @@ export default defineComponent({
       { prop: "cellphone", label: "手机号码", minWidth: "100" },
       { prop: "enable", label: "状态", minWidth: "100", slotName: "status" },
       { prop: "createAt", label: "创建时间", minWidth: "250", slotName: "createAt" },
-      { prop: "updateAt", label: "更新时间", minWidth: "250", slotName: "updateAt" }
+      { prop: "updateAt", label: "更新时间", minWidth: "250", slotName: "updateAt" },
+      { prop: "", label: "操作", minWidth: "120", slotName: "handler" }
     ]
+
+    const selectionChange = (data: any) => {
+      console.log("data:", data)
+    }
 
     return {
       searchFormConfig,
       userList,
       userCount,
-      propList
+      propList,
+      showIndexColumn,
+      showSelectColumn,
+      title,
+      selectionChange
     }
   }
 })
@@ -66,5 +108,9 @@ export default defineComponent({
 .content {
   padding: 20px;
   border-top: 20px solid #f0f2f5;
+}
+
+:deep(.el-button.is-link) {
+  color: #409eff;
 }
 </style>
